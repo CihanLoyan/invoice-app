eb54886ad69ee8e578926ac91f9c577610c32f33
CihanLoyan <cihanloyan@gmail.com>
2022-08-16 19:51:00 +0300

    Complete invoice component

--- src/components/Invoice.js
+++ src/components/Invoice.js
@@ -6,16 +6,25 @@ const Invoice = ({invoice}) => {
     <>
         <Link to={`/${invoice.invoiceNo}`} key={invoice.invoiceNo} >
             <div className='invoice'>
-                <div className='top'>
-                  <div className='invoice-no'>
-                    <span>Invoice Number:</span>
-                    <h4>{`${invoice.invoiceNo}`}</h4>
+                <div className='invoice-info'>
+                  <div className='invoice-no-status'>
+                    <div className='status'>
+                      <h4 className={invoice.status ? 'green' : 'red'}>
+                      {
+                        invoice.status ? 'Paid' : 'Not Paid'
+                      }
+                      </h4>
+                    </div>
+                    <div className='invoice-no'>
+                      <span>Invoice Number:</span>
+                      <h4>{`#${invoice.invoiceNo}`}</h4>
+                    </div>
                   </div>
                   <div className="companies">
                     <div className='from'>
                       <label>From:</label>
                       <span>
-                        {`${invoice.yourCompany}`}
+                        {`${invoice.from}`}
                       </span>
                     </div>
                     <div className='bill-to'>
@@ -40,8 +49,46 @@ const Invoice = ({invoice}) => {
                     </div>
                   </div>
                 </div>
-                <div className='center'>2</div>
-                <div className='bottom'>3</div>
+                <div className='invoice-detail'>
+                  <h4>{invoice.projectName}</h4>
+                  <p>{invoice.description}</p>
+                </div>
+                <div className='invoice-account'>
+                  <div className='unit-qty-ext'>
+                    <div className='unit-price'>
+                      <label>Unit Price:</label>
+                      <span>{`$${invoice.unitPrice}`}</span>
+                    </div>
+                    <div className='quantity'>
+                      <label>Quantity:</label>
+                      <span>{`${invoice.quantity}`}</span>
+                    </div>
+                    <div className='extras'>
+                      <label>Extras:</label>
+                      <span>{`$${invoice.extras}`}</span>
+                    </div>
+                  </div>
+                  <div className='total-pay'>
+                      <div className='payment-type'>
+                        <label>Payment Type:</label>
+                        <span>{`${invoice.paymentType}`}</span>
+                      </div>
+                      <div className='total-account'>
+                        <div className='sub-total'>
+                          <label>SubTotal:</label>
+                          <span>{`$${invoice.subTotal}`}</span>
+                        </div>
+                        <div className='tax'>
+                          <label>Tax:</label>
+                          <span>{`%${invoice.taxRate}`}</span>
+                        </div>
+                        <div className='total-price'>
+                          <label>TOTAL:</label>
+                          <span>{`$${invoice.totalPrice}`}</span>
+                        </div>
+                      </div>
+                  </div>
+                </div>
             </div>
         </Link>
     </>
--- src/db/data.js
+++ src/db/data.js
@@ -1,20 +1,20 @@
 const invoices = [
     {
+        status: true,
         invoiceNo: 123,
-        yourCompany: "Shift",
+        from: "Shift",
         billTo: "Onedio",
         invoiceDate: "15/08/2022",
         dueDate: "15/09/2022",
         projectName: "Content Management Desktop Application ",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
-        paymentType: "Credit Card",
-        unitPrice: "$10.000",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercise null pariatur. consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+        unitPrice: "10.000",
         quantity: 1,
-        extras: "$3.000",
-        subTotal: "$13.000",
-        taxRate: "%10",
-        total: "$11.700",
-        status: true
+        extras: "3.000",
+        subTotal: "13.000",
+        taxRate: "10",
+        totalPrice: "14.300",
+        paymentType: "Credit Card",
     }
 ];
 
