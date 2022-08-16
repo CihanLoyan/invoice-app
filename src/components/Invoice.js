f8e4b5a62513da5e6ed41cdadf79886e7dc52cd3
CihanLoyan <cihanloyan@gmail.com>
2022-08-16 09:43:49 +0300

    Edit invoice component

--- src/components/Invoice.js
+++ src/components/Invoice.js
@@ -6,7 +6,42 @@ const Invoice = ({invoice}) => {
     <>
         <Link to={`/${invoice.invoiceNo}`} key={invoice.invoiceNo} >
             <div className='invoice'>
-                {invoice.customerName}
+                <div className='top'>
+                  <div className='invoice-no'>
+                    <span>Invoice Number:</span>
+                    <h4>{`${invoice.invoiceNo}`}</h4>
+                  </div>
+                  <div className="companies">
+                    <div className='from'>
+                      <label>From:</label>
+                      <span>
+                        {`${invoice.yourCompany}`}
+                      </span>
+                    </div>
+                    <div className='bill-to'>
+                      <label>Bill To:</label>
+                      <span>
+                        {invoice.billTo}
+                      </span>
+                    </div>
+                  </div>
+                  <div className="date">
+                    <div className='invoice-date'>
+                      <label>Invoice Date:</label>
+                      <span>
+                        {invoice.invoiceDate}
+                      </span>
+                    </div>
+                    <div className="due-date">
+                      <label>Due Date:</label>
+                      <span>
+                        {invoice.dueDate}
+                      </span>
+                    </div>
+                  </div>
+                </div>
+                <div className='center'>2</div>
+                <div className='bottom'>3</div>
             </div>
         </Link>
     </>
--- src/db/data.js
+++ src/db/data.js
@@ -1,19 +1,20 @@
 const invoices = [
     {
         invoiceNo: 123,
-        customerName: "Onedio",
+        yourCompany: "Shift",
+        billTo: "Onedio",
         invoiceDate: "15/08/2022",
         dueDate: "15/09/2022",
-        paymentType: "Credit Card",
-        status: true,
         projectName: "Content Management Desktop Application ",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
+        paymentType: "Credit Card",
         unitPrice: "$10.000",
         quantity: 1,
         extras: "$3.000",
         subTotal: "$13.000",
         taxRate: "%10",
-        total: "$11.700"
+        total: "$11.700",
+        status: true
     }
 ];
 
